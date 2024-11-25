8db0c7f9624c213d57c6e32d97947591a573a8af
Adica <bolotovakadica455@gmail.com>
2024-11-25 12:08:51 +0600

    Валидация локаций

--- main.py
+++ main.py
@@ -12,3 +12,4 @@ while True:
     print("Enter the third location and weight:")
     guess3 = int(input())
     weight3 = int(input())
+    if guess1 != guess2 and guess1 != guess3 and guess2 != guess3:
