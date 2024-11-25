06246e5445b13ceb33a2c85a87c3f81864ebd298
Adica <bolotovakadica455@gmail.com>
2024-11-25 12:11:07 +0600

    Итоговая версия 1.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
-
 locations = [random.randint(1, 7) for _ in range(3)]
+print("Guess the locations of the 3 boxes and their weights. Enter one location and its weight at a time.")
 
 while True:
     print("Enter the first location and weight:")
@@ -12,11 +12,13 @@ while True:
     print("Enter the third location and weight:")
     guess3 = int(input())
     weight3 = int(input())
+
     if guess1 != guess2 and guess1 != guess3 and guess2 != guess3:
         correct_guesses = 0
         for g in [guess1, guess2, guess3]:
             if g in locations:
                 correct_guesses += 1
+
         if correct_guesses == 3:
             total_weight = weight1 + weight2 + weight3
             if total_weight == 713:
