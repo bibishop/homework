65559d026e32b9efbe4e3f0b1ce816b16523a5f1
Adica <bolotovakadica455@gmail.com>
2024-11-25 12:07:05 +0600

    Добавление инпутов

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import random
 
 locations = [random.randint(1, 7) for _ in range(3)]
+
+while True:
+    print("Enter the first location and weight:")
+    guess1 = int(input())
+    weight1 = int(input())
