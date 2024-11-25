f04822df3813bcde30251badcb8f7861b80a6d5a
Adica <bolotovakadica455@gmail.com>
2024-11-25 12:09:21 +0600

    Подсчет попыток

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ while True:
     guess3 = int(input())
     weight3 = int(input())
     if guess1 != guess2 and guess1 != guess3 and guess2 != guess3:
+        correct_guesses = 0
+        for g in [guess1, guess2, guess3]:
+            if g in locations:
+                correct_guesses += 1
